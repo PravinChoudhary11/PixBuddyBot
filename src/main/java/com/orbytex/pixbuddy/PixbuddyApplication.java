61cc2691137015821b33fb65a24456ce9bdcb2a4
PravinChoudhary11 <itspracin750@gmail.com>
2026-03-25 02:04:31 +0530

    fix : render compatable deploy

--- src/main/java/com/orbytex/pixbuddy/PixbuddyApplication.java
+++ src/main/java/com/orbytex/pixbuddy/PixbuddyApplication.java
@@ -6,8 +6,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PixbuddyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PixbuddyApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        System.out.println("=================================");
+        System.out.println("PixBuddy Early Debug");
+        System.out.println("=================================");
+
+        System.out.println("DISCORD_TOKEN: " +
+                (System.getenv("DISCORD_TOKEN") != null));
+
+        System.out.println("GEMINI_KEY1: " +
+                (System.getenv("SPRING_GEMINI_PIXBUDDY_APIKEY1") != null));
+
+        System.out.println("GEMINI_KEY2: " +
+                (System.getenv("SPRING_GEMINI_PIXBUDDY_APIKEY2") != null));
+
+        System.out.println("GEMINI_KEY3: " +
+                (System.getenv("SPRING_GEMINI_PIXBUDDY_APIKEY3") != null));
+
+        System.out.println("=================================");
+
+        SpringApplication.run(PixbuddyApplication.class, args);
+    }
 }
